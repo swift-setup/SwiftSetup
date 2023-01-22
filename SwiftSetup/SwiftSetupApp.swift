d9ffe6ef87086cc1d44e0ead56d9f1e7312f3039
sirily11 <sirily1997@gmail.com>
2023-01-23 02:15:33 +0800

    Initial Commit

---
+++ SwiftSetup/SwiftSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSetupApp.swift
+//  SwiftSetup
+//
+//  Created by Qiwei Li on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

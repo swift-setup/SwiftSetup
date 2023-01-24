4598b6965dc81bbcc5031082b3ae756160b110d3
sirily11 <32106111+sirily11@users.noreply.github.com>
2023-01-24 21:18:40 +0800

    fix: use dmg instead (#4)

--- scripts/notary.sh
+++ scripts/notary.sh
@@ -1,9 +1,9 @@
 # Create variable name for the app name
 APP_NAME="SwiftSetup"
 
-zip -r $APP_NAME.zip $APP_NAME.app
-# copy the archive to root
-cp release.xcarchive/Products/Applications/$APP_NAME.zip .
+# create dmg
+create-dmg $APP_NAME.app
+CP *.dmg $APP_NAME.dmg
 
 # notarize the app
-xcrun notarytool submit $APP_NAME.zip --apple-id "$APPLE_ID" --team-id "$APPLE_TEAM_ID" --password "$APPLE_ID_PWD"
+xcrun notarytool submit $APP_NAME.dmg --apple-id "$APPLE_ID" --team-id "$APPLE_TEAM_ID" --password "$APPLE_ID_PWD"

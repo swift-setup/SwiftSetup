e284f9b31f0e0277bb6f00657106318e12df22c2
sirily11 <sirily1997@gmail.com>
2023-01-29 00:54:35 +0800

    fix: macos ci

--- scripts/notary.sh
+++ scripts/notary.sh
@@ -6,4 +6,7 @@ create-dmg $APP_NAME.app
 cp *.dmg $APP_NAME.dmg
 
 # notarize the app
-xcrun notarytool submit $APP_NAME.dmg --apple-id "$APPLE_ID" --team-id "$APPLE_TEAM_ID" --password "$APPLE_ID_PWD"
+xcrun notarytool submit $APP_NAME.dmg --apple-id "$APPLE_ID" --team-id "$APPLE_TEAM_ID" --password "$APPLE_ID_PWD" --wait
+
+# staple the ticket
+xcrun stapler staple $APP_NAME.dmg
